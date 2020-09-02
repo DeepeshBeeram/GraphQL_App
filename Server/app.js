e2f9d17b0c0ce427ece1475352db9d99ffbd6572
DeepeshBeeram <deepesh.kp62@gmail.com>
2020-09-02 15:15:52 -0400

    Backend Server(Node JS, Express, MongoDb)

---
+++ Server/app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
+const mongoose = require("mongoose");
+
+const app = express();
+
+mongoose.connect(
+  "mongodb+srv://Meher_IS603:Beeram@123@cluster-is603.pwuby.mongodb.net/GraphQL-DB?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+
+mongoose.connection.once("open", () => {
+  console.log("connected to databse");
+});
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
+
+app.listen(4000, () => {
+  console.log("Listening for requests on port 4000");
+});
